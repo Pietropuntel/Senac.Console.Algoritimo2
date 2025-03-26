8b05d4c2a87af325a9cf60cf03f3f0f6e35ff434
Pietro Puntel <pietrosamueldossantospuntel@gmail.com>
2025-03-25 21:50:32 -0300

    leituras de variaveis

--- algoritimo2/algoritimo2/Program.cs
+++ algoritimo2/algoritimo2/Program.cs
@@ -3,5 +3,15 @@ string nomeCliente = "Pietro Puntel";
 decimal preco = 20M;
 char letra = 'a';
 var nomeCompleto = "Pietro Samuel dos Santos Puntel";
-var altura = 175; 
-
+var altura = 175;
+Console.WriteLine("Digite o seu nome:");
+nomeCompleto = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Bem vindo" + nomeCompleto);
+Console.Write(" Digite sua idade ");
+var linha = Console.ReadLine);
+idade = int.Parse(linha);
+Console.WriteLine("idade informada" + idade );
+if (idade < 18)
+    Console.WriteLine("Você não pode estar aqui !");
+Console.ReadKey();
